d59c63ade08a83f2a323758782be5a603b65a034
dabadema <dabadema7@hotmail.com>
2023-10-07 20:26:18 +0200

    Minor change to commit

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -14,7 +14,7 @@ class errorBoundry extends Component {
 
   render() {
     if (this.state.hasError) {
-      return <h1>Ooooops. That is not good</h1>;
+      return <h1>Ooooops. That is not good at all</h1>;
     }
     return this.props.children;
   }
